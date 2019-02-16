a00e7fc1250be57e37772b76bda28d2f43760a5f
evan hemsley <evan.hemsley@gmail.com>
2019-02-15 16:22:42 -0800

    accidentally redefined size

--- src/gc_optimized_map.ts
+++ src/gc_optimized_map.ts
@@ -1,6 +1,4 @@
 export class GCOptimizedMap<TKey, TValue> extends Map<TKey, TValue> {
-    public size: number;
-
     public clear(): void {
         for (const [k, v] of this) {
             this.delete(k);
