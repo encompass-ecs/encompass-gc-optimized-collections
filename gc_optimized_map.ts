e65daac9ced6fd07f78e4ee2a1c68e5d8a52a75b
evan hemsley <evan.hemsley@gmail.com>
2019-02-15 17:22:28 -0800

    rewrite to make use of lualib map

--- gc_optimized_map.ts
+++ gc_optimized_map.ts
@@ -1,8 +1,58 @@
-export class GCOptimizedMap<TKey, TValue> extends Map<TKey, TValue> {
+export class GCOptimizedMap<TKey, TValue> {
+    get size(): number {
+        return this.internalMap.size;
+    }
+
+    private internalMap: Map<TKey, TValue>;
+
+    constructor(other?: Iterable<[TKey, TValue]> | Array<[TKey, TValue]>) {
+        this.internalMap = new Map(other);
+    }
+
     public clear(): void {
-        for (const [k, v] of this) {
+        for (const [k, v] of this.internalMap) {
             this.delete(k);
         }
         return;
+    }
+
+    public delete(key: TKey): boolean {
+        return this.internalMap.delete(key);
+    }
+
+    public [Symbol.iterator](): IterableIterator<[TKey, TValue]> {
+        return this.entries();
+    }
+
+    public entries(): IterableIterator<[TKey, TValue]> {
+        return this.internalMap.entries();
+    }
+
+    public forEach(callback: (value: TValue, key: TKey, map: GCOptimizedMap<TKey, TValue>) => any): void {
+        for (const [k, v] of this.internalMap) {
+            callback(v, k, this);
+        }
+        return;
+    }
+
+    public get(key: TKey): TValue {
+        return this.internalMap.get(key);
+    }
+
+    public has(key: TKey): boolean {
+        return this.internalMap.has(key);
+    }
+
+    public keys(): IterableIterator<TKey> {
+        return this.internalMap.keys();
+    }
+
+    public set(key: TKey, value: TValue): GCOptimizedMap<TKey, TValue> {
+        this.internalMap.set(key, value);
+        return this;
+    }
+
+    public values(): IterableIterator<TValue> {
+        return this.internalMap.values();
     }
 }
--- test/gc_optimized_map_spec.ts
+++ test/gc_optimized_map_spec.ts
@@ -1,6 +1,48 @@
 import { GCOptimizedMap } from "../gc_optimized_map";
 
 describe("GCOptimizedMap", () => {
+    describe("constructor", () => {
+        describe("no argument", () => {
+            const myMap = new GCOptimizedMap();
+
+            it("should have size 0", () => {
+                expect(myMap.size).toBe(0);
+            });
+        });
+
+        describe("iterable argument", () => {
+            const myMap = new GCOptimizedMap(new Map([["a", "c"],["b", "d"]]));
+
+            it("should have size 2", () => {
+                expect(myMap.size).toBe(2);
+            });
+
+            it("should have element a", () => {
+                expect(myMap.has("a")).toBeTruthy();
+            });
+
+            it("should have element b", () => {
+                expect(myMap.has("b")).toBeTruthy();
+            });
+        });
+
+        describe("GCOptimizedMap argument", () => {
+            const myMap = new GCOptimizedMap(new GCOptimizedMap([["a", "c"],["b", "d"]]));
+
+            it("should have size 2", () => {
+                expect(myMap.size).toBe(2);
+            });
+
+            it("should have element a", () => {
+                expect(myMap.has("a")).toBeTruthy();
+            });
+
+            it("should have element b", () => {
+                expect(myMap.has("b")).toBeTruthy();
+            });
+        });
+    });
+
     describe("clear", () => {
         const myMap = new GCOptimizedMap<string, string>([["a","c"], ["b","d"]]);
 
@@ -17,5 +59,163 @@ describe("GCOptimizedMap", () => {
         it("should not have second element", () => {
             expect(myMap.has("b")).toBeFalsy();
         });
+    });
+
+    describe("delete", () => {
+        const myMap = new GCOptimizedMap(new Map([["a", "c"],["b", "d"]]));
+
+        myMap.delete("a");
+
+        it("should not have element a", () => {
+            expect(myMap.has("a")).toBeFalsy();
+        });
+
+        it("should have element b", () => {
+            expect(myMap.has("b")).toBeTruthy();
+        });
+    });
+
+    describe("entries", () => {
+        const myMap = new GCOptimizedMap([[5, 2], [6, 3], [7, 4]]);
+        let count = 0;
+        for (const [key, value] of myMap.entries()) {
+            count += key + value;
+        }
+
+        it("should iterate all keys and values", () => {
+            expect(count).toBe(27);
+        });
+    });
+
+    describe("forEach", () => {
+        const myMap = new GCOptimizedMap([["a", 2], ["b", 3], ["c", 4]]);
+        let count = 0;
+        myMap.forEach(i => count += i);
+
+        it("iterates over all entries", () => {
+            expect(count).toBe(9);
+        });
+    });
+
+    describe("get", () => {
+        const myMap = new GCOptimizedMap([["a", "c"], ["b", "d"]]);
+
+        describe("the map has the key", () => {
+            it("gets the value from the key", () => {
+                expect(myMap.get("a")).toBe("c");
+            });
+        });
+
+        describe("the map does not have the key", () => {
+            it("returns undefined", () => {
+                expect(myMap.get("c")).toBeUndefined();
+            });
+        });
+    });
+
+    describe("has", () => {
+        const myMap = new GCOptimizedMap([["a", "c"]]);
+
+        describe("the map has the key", () => {
+            it("returns true", () => {
+                expect(myMap.has("a")).toBeTruthy();
+            });
+        });
+
+        describe("the map does not have the key", () => {
+            it("returns false", () => {
+                expect(myMap.has("c")).toBeFalsy();
+            });
+        });
+
+        describe("the argument is null", () => {
+            it("returns false", () => {
+                expect(myMap.has(null)).toBeFalsy();
+            });
+        });
+    });
+
+    describe("keys", () => {
+        const myMap = new GCOptimizedMap([[5, 2], [6, 3], [7, 4]]);
+        let count = 0;
+        for (const key of myMap.keys()) {
+            count += key;
+        }
+
+        it("iterates over keys", () => {
+            expect(count).toBe(18);
+        });
+    });
+
+    describe("set", () => {
+        const myMap = new GCOptimizedMap();
+
+        myMap.set("a", 5);
+
+        it("should have key", () => {
+            expect(myMap.has("a")).toBeTruthy();
+        });
+
+        it("should have value", () => {
+            expect(myMap.get("a")).toBe(5);
+        });
+    });
+
+    describe("values", () => {
+        const myMap = new GCOptimizedMap([[5, 2], [6, 3], [7, 4]]);
+        let count = 0;
+        for (const value of myMap.values()) {
+            count += value;
+        }
+
+        it("iterates over all values", () => {
+            expect(count).toBe(9);
+        });
+    });
+
+    describe("size", () => {
+        describe("the map is empty", () => {
+            const m = new GCOptimizedMap();
+
+            it("returns 0", () => {
+                expect(m.size).toBe(0);
+            });
+        });
+
+        describe("the map has a value set", () => {
+            const m = new GCOptimizedMap();
+
+            m.set(1, 3);
+
+            it("returns 1", () => {
+                expect(m.size).toBe(1);
+            });
+        });
+
+        describe("the map is constructed with 2 entries", () => {
+            const m = new GCOptimizedMap([[1,2],[3,4]]);
+
+            it("returns 2", () => {
+                expect(m.size).toBe(2);
+            });
+        });
+
+        describe("the map is cleared", () => {
+            const m = new GCOptimizedMap([[1,2],[3,4]]);
+            m.clear();
+
+            it("returns 0", () => {
+                expect(m.size).toBe(0);
+            });
+        });
+
+        describe("the map has 2 entries and 1 is deleted", () => {
+            const m = new GCOptimizedMap([[1,2], [3,4]]);
+            m.delete(1);
+
+            it("returns 1", () => {
+                expect(m.size).toBe(1);
+            });
+        })
     });
 });
